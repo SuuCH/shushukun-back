39dabdc31e1a9d85c8b3eccbeac4dc59071e54e0
SuuCH <ntrikrik1027@gmail.com>
2022-01-27 06:49:55 +0900

    feat: dBの全データを取得するget_submitsを作成

--- api/cruds/submit.py
+++ api/cruds/submit.py
@@ -1,4 +1,9 @@
+from multiprocessing.spawn import import_main_path
+from typing import List, Tuple
+
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 
 import api.models.submit as submit_model
 import api.schemas.submit as submit_schema
@@ -11,3 +16,16 @@ async def create_submit(
   await db.commit()
   await db.refresh(submit)
   return submit
+
+async def get_submits_with_done(db: AsyncSession) -> List[Tuple[int, str, str ,bool]]:
+  result: Result = await(
+    db.execute(
+      select(
+        submit_model.Submit.id,
+        submit_model.Submit.src,
+        submit_model.Submit.error,
+        submit_model.Done.id.isnot(None).label("done"),
+      ).outerjoin(submit_model.Done)
+    )
+  )
+  return result.all()
