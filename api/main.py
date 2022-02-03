0bb6ec702cc5bd0911c9b7a904328929e626addf
SuuCH <ntrikrik1027@gmail.com>
2022-02-03 09:08:56 +0900

    feat: CORSを仮設定

--- api/main.py
+++ api/main.py
@@ -1,10 +1,17 @@
-import imp
 from fastapi import FastAPI
 
 from api.routers import submit, done
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app = FastAPI()
 app.include_router(submit.router)
 app.include_router(done.router)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = ["*"],
+  allow_credentials = True,
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
